c3e8175f5e340522f0350ec3d43266f749957db1
Edward Heaver <edward.heaver01@gmail.com>
2023-07-16 15:43:18 +0100

    Test parser with realistic input

--- src/test/scala/parser/CronParserTest.scala
+++ src/test/scala/parser/CronParserTest.scala
@@ -16,10 +16,14 @@ class CronParserTest extends AnyFlatSpec {
     ))
   }
 
-  it should "respect that some intervals include 0" in {
-    val cron: Cron = Cron("0", "0", "0", "0", "0")
-    assertThrows[RuntimeException] {
-      CronParser.execute(cron)
-    }
+  it should "be able to pass a cron that is realistic" in {
+    val cron: Cron = Cron("1-15", "*/5", "1,15", "7", "*")
+    assert(CronParser.execute(cron) == List(
+      ParsedCron(TimeIntervals.Minute, (1 to 15).toList),
+      ParsedCron(TimeIntervals.Hour, List(0, 5, 10, 15, 20)),
+      ParsedCron(TimeIntervals.DayOfMonth, List(1, 15)),
+      ParsedCron(TimeIntervals.Month, List(7)),
+      ParsedCron(TimeIntervals.DayOfWeek, (1 to 7).toList),
+    ))
   }
 }
