86602621fe1f68696343f1446030cf13a471ae9d
Edward Heaver <edward.heaver01@gmail.com>
2023-07-21 14:59:15 +0100

    Tried for comprehension. Couldn't quite make it work. Split the Trys and Matches

--- src/main/scala/CronParserApp.scala
+++ src/main/scala/CronParserApp.scala
@@ -15,20 +15,26 @@ object CronParserApp extends CommandApp(
     val cronArg = Opts.arguments[String](metavar = "cron expression")
     val outputFilePath = Opts.option[String]("output-file", help = "Path to write cron expression").withDefault("")
 
-    (cronArg, outputFilePath).mapN {(cron: NonEmptyList[String], outPath: String) =>
+    (cronArg, outputFilePath).mapN { (cron: NonEmptyList[String], outPath: String) =>
       val c = cron.take(5)
       val cObj = Cron.fromList(c)
       val parsedCrons = CronParser.execute(cObj)
 
       if outPath.isEmpty then CronPrinter.print(parsedCrons)
-      else Try(new File(outPath)) match {
-        case Success(v) =>
-          Try(CronPrinter.print(parsedCrons, v)) match {
-            case Success(_) => println(s"Wrote output to file: ${v.getCanonicalPath}")
+      else
+        val file: Try[File] = Try(new File(outPath)) match {
+          case Success(v) => Success(v)
+          case Failure(e) =>
+            println(s"Invalid path given. Got error: $e")
+            Failure(e)
+        }
+
+        if file.isSuccess then {
+          Try(CronPrinter.print(parsedCrons, file.get)) match {
+            case Success(_) => println(s"Wrote output to file: ${file.get.getCanonicalPath}")
             case Failure(e) => println(s"Error occurred while writing file. Got error: $e")
           }
-        case Failure(e) => println(s"Invalid path given. Got error: $e")
-      }
+        }
     }
   }
 )
